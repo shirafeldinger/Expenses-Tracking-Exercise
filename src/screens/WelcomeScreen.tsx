d2e94aa0c90f1dcba93d3ca5b55e706610b652bf
Shira Feldinger <shiraf@yad2.co.il>
2024-12-19 16:52:00 +0200

    no inline style

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,46 +1,59 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState } from 'react';
-import { View, Text, Button, TextInput, Alert } from 'react-native';
+import { View, Text, Button, TextInput, Alert, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const WelcomeScreen = ({ navigation }) => {
   const [fullName, setFullName] = useState<string>('');
-  const navigateToHome = () => {
-    navigation.reset({
-        index: 0, // Set the index of the screen to the first screen in the stack, : We set the HomeTabs as the first screen in the stack
-        routes: [{ name: 'HomeTabs' }], // Define the new stack with only HomeTabs
-      });
-  };
 
   const handleSubmit = async () => {
     if (fullName.trim()) {
       await AsyncStorage.setItem('fullName', fullName);
-      navigateToHome();
+      // Ensure the navigation.replace works as expected
+      navigation.replace('HomeTabs');
     } else {
       Alert.alert('Error', 'Please enter your full name.');
     }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>Welcome to the App!</Text>
-      <Text style={{ fontSize: 16, marginBottom: 10 }}>Please enter your full name:</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Welcome to the App!</Text>
+      <Text style={styles.subHeading}>Please enter your full name:</Text>
       <TextInput
         value={fullName}
         onChangeText={setFullName}
         placeholder="Full Name"
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-          marginBottom: 20,
-          paddingHorizontal: 10,
-          width: '100%',
-          borderRadius: 5,
-        }}
+        style={styles.input}
       />
       <Button title="Submit" onPress={handleSubmit} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  subHeading: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '100%',
+    borderRadius: 5,
+  },
+});
 
 export default WelcomeScreen;
